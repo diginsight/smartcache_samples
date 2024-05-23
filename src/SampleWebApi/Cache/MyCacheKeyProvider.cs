4d47c2d44d8684ef63b60d0d668c9ec5c06bde18
DARIO AIROLDI <dario.airoldi@it.abb.com>
2024-05-23 10:01:03 -0700

    versions update

--- src/SampleWebApi/Cache/MyCacheKeyProvider.cs
+++ src/SampleWebApi/Cache/MyCacheKeyProvider.cs
@@ -5,7 +5,20 @@ using Diginsight.SmartCache;
 
 public sealed class MyCacheKeyProvider : ICacheKeyProvider
 {
-    public ToKeyResult ToKey(ICacheKeyService service, object? obj)
+    //public ToKeyResult ToKey(ICacheKeyService service, object? obj)
+    //{
+    //    switch (obj)
+    //    {
+    //        //case IGroup g:
+    //        //    var key = new ToKeyResult(g.Id);
+    //        //    return key;
+
+    //        default:
+    //            return default;
+    //    }
+    //}
+
+    object? ICacheKeyProvider.ToKey(ICacheKeyService service, object? obj)
     {
         switch (obj)
         {
